d55778afad35f5c4951437bfc77f0cbdbd49e640
Pedro <pedrokehl@hotmail.com>
2019-05-19 23:25:02 -0300

    Implement answer of questions on transaction guide

---
+++ src/guide/transaction-guide.ts
@@ -0,0 +1,20 @@
+import answersRepository from "../repositories/answers-repository";
+import NoteFactory from "./notes/note-factory";
+
+class TransactionGuide {
+  private readonly typedNotes: string[];
+
+  constructor(typedNotes: string[] = []) {
+    this.typedNotes = typedNotes;
+  }
+
+  public process(): string[] {
+    this.typedNotes.forEach((typedNote) => {
+      const note = NoteFactory.getNote(typedNote.trim());
+      note.process();
+    });
+    return answersRepository.getAll();
+  }
+}
+
+export default TransactionGuide;
